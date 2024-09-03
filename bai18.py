1cf8ded3451bc36b78b2c1ee84ffc499a6ca16a3
myduyen17 <duyenmylethi987@gmail.com>
2024-09-03 21:23:46 +0700

    Add files via upload

--- bai18.py
+++ bai18.py
@@ -16,8 +16,10 @@ B = d*3600+e*60+f
 print("B = ",B,"s")
 C1 = (A + B) 
 print("C1 = ",C1,"s")
+print("Tổng của hai giờ là :",C1//3600,"giờ",C1%3600//60,"phút",C1%60,"giây")
 C2 = A - B
 if A < B:
     C2 = B - A
 print("C2 = ",C2,"s")
+print("Hiệu của hai giờ là :",C2//3600,"giờ",C2%3600//60,"phút",C2%60,"giây")
 
